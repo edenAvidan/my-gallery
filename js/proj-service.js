5b68abe9b64134b9830de2cafbd74cd127c6f89e
Eden Avidan <edenaavidan@gmail.com>
2022-02-07 16:44:54 +0200

    added more accurate info about projects

--- js/proj-service.js
+++ js/proj-service.js
@@ -14,12 +14,12 @@ function getProjById(projId) {
 
 function _createProjs() {
     gProjs = [
-        _createProj('minesweeper', 'Minesweeper', 'Classic Minesweeper', 'Should be lorem', '1.29.2022', ['Matrixes', 'mouse events']),
-        _createProj('pacman', 'Pacman', 'Classic Pacman', 'Should be lorem2', '1.25.2022', ['Matrixes', 'keyboard events']),
-        _createProj('book-shop', 'Book Shop', 'A Book Shop', 'Should be lorem2', '1.25.2022', ['Matrixes', 'keyboard events']),
-        _createProj('chess', 'Chess', 'Classic Chess', 'Should be lorem2', '1.25.2022', ['Matrixes', 'keyboard events']),
-        _createProj('guess-me', 'GuessMe', 'Think Of Someone And I Will Guess It', 'Should be lorem2', '1.25.2022', ['Matrixes', 'keyboard events']),
-        _createProj('ballon-pop', 'Ballon Pop', 'It\'s Like Bubble Wrap', 'Should be lorem2', '1.25.2022', ['Matrixes', 'keyboard events']),
+        _createProj('minesweeper', 'Minesweeper', 'Classic Minesweeper', makeLorem(), '1/29/2022', ['Matrixes', 'mouse events']),
+        _createProj('pacman', 'Pacman', 'Classic Pacman', makeLorem(), '1/25/2022', ['Matrixes', 'keyboard events']),
+        _createProj('book-shop', 'Book Shop', 'A Book Shop', makeLorem(), '2/5/2022', ['CRUD']),
+        _createProj('chess', 'Chess', 'Classic Chess', makeLorem(), '1/20/2022', ['Matrixes', 'mouse events']),
+        _createProj('guess-me', 'GuessMe', 'Think Of Someone And I Will Guess It', makeLorem(), '2/6/2022', ['jQuery', 'Bootstrap']),
+        _createProj('ballon-pop', 'Ballon Pop', 'It\'s Like Bubble Wrap', makeLorem(), '1/20/2022', ['Arrays']),
     ]
 }
 
@@ -29,7 +29,7 @@ function _createProj(id, name, title, desc, publishedAt, labels) {
         name: name,
         title: title,
         desc: desc,
-        url: `projs/${id}`,
+        url: `projs/${name}`,
         publishedAt: publishedAt,
         labels: labels
     }
